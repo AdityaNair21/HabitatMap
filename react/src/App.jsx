23258dba1be80816f398855f106271d8b8cd74d5
Aditya Nair <nair.aditya.r@gmail.com>
2024-10-08 19:19:48 -0700

    Your descriptive commit message here

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,22 +1,29 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
-import Home from "./components/Home";
-import PostUser from "./components/PostUser";
-import GetAllUser from "./components/GetAllUser";
+import Login from "./components/Login";
+import MapPage from "./components/MapPage";
+import AnimalPage from "./components/AnimalPage";
+import Dashboard from "./components/Dashboard"
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="post" element={<PostUser />} />
-          <Route path="get" element={<GetAllUser />} />
-        </Route>
+        {/* Login page, user starts here */}
+        <Route path="/login" element={<Login />} />
+
+        {/* Home page */}
+        <Route path="/" element={<Dashboard />} />
+
+        {/* Map page */}
+        <Route path="/map" element={<MapPage />} />
+
+        {/* Animal page with dynamic id */}
+        <Route path="/animal/:id" element={<AnimalPage />} />
       </Routes>
     </BrowserRouter>
   );
 }
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
---
+++ react/src/components/AnimalPage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { Container, Typography } from '@mui/material';
+
+export default function AnimalPage() {
+    // Get the animal ID (name) from the URL parameter
+    const { id } = useParams();
+
+    // Capitalize the first letter of the animal name (optional)
+    const animalName = id.charAt(0).toUpperCase() + id.slice(1);
+
+    return (
+        <Container>
+            <Typography variant="h4" align="center" sx={{ marginTop: 4 }}>
+                {animalName} page goes here
+            </Typography>
+        </Container>
+    );
+}
