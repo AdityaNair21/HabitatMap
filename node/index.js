db8349761db430fb0650e662d0072df4dbaf248e
Dhruv <61105515+dbiscoding@users.noreply.github.com>
2024-10-21 01:42:53 -0700

    Added a dummy RESTful API route for getting all reports in JSON format and  task file with Restful endpoints

--- node/index.js
+++ node/index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-
+// Define the route for the root path
+// Restful API
+app.get('/reports', (req, res) => {
+  res.send('All reports will be returned in json format using res.json()');
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
