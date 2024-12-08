d8ab9fd6e194496bcd9f4acded771c55ad06dd9d
Dhruv <61105515+dbiscoding@users.noreply.github.com>
2024-12-07 16:03:25 -0800

    Refactor report model and routes: Cleaned up report routes by removing commented-out code and removed unused report model

--- node/models/report.js
+++ node/models/report.js
@@ -2,15 +2,6 @@ const mongoose = require('mongoose');
 
 // Define the schema for a report
 const ReportSchema = new mongoose.Schema({
-    userId: {type: String, required: true},
-    speciesId: {type: String, required: true},
-    location: { latitude : {type: Number, required: true}, longitude : {type: Number, required: true}},
-    description: {type: String},
-    }, 
-    {timestamps: true});
-
-// Define the schema for a report
-const ReportSchema2 = new mongoose.Schema({
     // user aggregate 
     user: {
         userId: {type: String, required: true},
@@ -35,6 +26,6 @@ const ReportSchema2 = new mongoose.Schema({
 {timestamps: true});
 
 // Create a model from the schema
-const Report = mongoose.model('Report', ReportSchema2);
+const Report = mongoose.model('Report', ReportSchema);
 
 module.exports = { Report };
